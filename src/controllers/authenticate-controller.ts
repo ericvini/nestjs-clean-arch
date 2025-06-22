998499421b92c72cd3fbe38f97c0010f14b18a42
ericvini <ericcobain3@gmail.com>
2025-06-22 17:56:00 -0300

    feat: autenticação com jwt e controller de autenticação

---
+++ src/controllers/authenticate-controller.ts
@@ -0,0 +1,50 @@
+import { UnauthorizedException, UsePipes } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { z } from 'zod';
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { compare } from 'bcryptjs';
+
+const authenticateBodySchema = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type AuthenticateBodySchema = z.infer<typeof authenticateBodySchema>;
+
+@Controller('/sessions')
+export class AuthenticateController {
+  constructor(
+    private jwt: JwtService,
+    private prisma: PrismaService,
+  ) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(authenticateBodySchema))
+  async handle(@Body() body: AuthenticateBodySchema) {
+    const { email, password } = body;
+
+    const user = await this.prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const isPasswordValid = await compare(password, user.password);
+
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+
+    const accesToken = this.jwt.sign({
+      sub: user.id,
+    });
+
+    return {
+      access_token: accesToken,
+    };
+  }
+}
